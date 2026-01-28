9765f42f70f38c392dadb0d864081e389d90a21e
Anthony Charles <anthonylannn@gmail.com>
2026-01-28 16:04:52 +0100

    links updated

--- src/sample.ts
+++ src/sample.ts
@@ -1,18 +1,15 @@
 const author = {
-    name: `Angela`,
-    nickname: `angie`,
-    tiktok: `https://www.tiktok.com/@codetonero`,
-    ig: `https://www.instagram.com/codetonero/`,
-    x: `https://x.com/codetonero`,
-    email: 'mailto:anthonylannn@gmail.com'
-
-}
-
+  name: `Angela`,
+  nickname: `angie`,
+  tiktok: `https://www.tiktok.com/@indieteamofone`,
+  ig: `https://www.instagram.com/indieteamofone/`,
+  x: `https://x.com/indieteamofone`,
+  email: "mailto:anthonylannn@gmail.com",
+};
 
 const buy = {
-    title: `See on Github`,
-    link: `https://github.com/anthonylan/angie`
-}
+  title: `See on Github`,
+  link: `https://github.com/anthonylan/angie`,
+};
 
-
-export { author, buy }
+export { author, buy };
